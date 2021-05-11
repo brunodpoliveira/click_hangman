48aa73cf38cb0d45466f9e8b04e52880167fed8d
Bruno <brunodp.oliveira@gmail.com>
2021-05-11 15:58:28 -0300

    Adding explanations to code
    Just adding explanations as to what each piece of the code does

--- hangman.py
+++ hangman.py
@@ -4,11 +4,14 @@ from words import word_list
 
 
 def get_word():
+    # get random word from list
     word = random.choice(word_list)
     return word.upper()
 
 
 def play(word):
+    # game will let you guess the entire word or a letter at a time
+    # converts all word chars to underscores
     word_completion = "_" * len(word)
     guessed = False
     guessed_letters = []
@@ -18,26 +21,37 @@ def play(word):
     print(display_hangman(tries))
     print(word_completion)
     print("\n")
+    # game loop active as long as the word is not guessed and there are more than 0 tries
     while not guessed and tries > 0:
         guess = input("Please guess a letter or a word: ").upper()
+        # if the user typed one letter
         if len(guess) == 1 and guess.isalpha():
+            # if they already guessed the letter
             if guess in guessed_letters:
                 print("You already guessed the letter", guess)
+            # if letter's not in the word
             elif guess not in word:
                 print(guess, "is not in the word")
                 tries -= 1
                 guessed_letters.append(guess)
+            # if letter's in the word
             else:
                 print("Good job,", guess, "is in the word")
                 guessed_letters.append(guess)
+                # converts word completion to list, so we can index into it
                 word_as_list = list(word_completion)
+                # enumerate on word so we can get both index i and letter at index for each iteration
                 indices = [i for i, letter in enumerate(word) if letter == guess]
+                # replaces each _ with the letter guessed
                 for index in indices:
                     word_as_list[index] = guess
+                # converts back to string
                 word_completion = "".join(word_as_list)
+                # if there are no underscores left, that means all letters were guessed
                 if "_" not in word_completion:
                     guessed = True
 
+        # same logic as above, but in case user tries to guess the entire word
         elif len(guess) == len(word) and guess.isalpha():
             if guess in guessed_words:
                 print("You've already guessed the word", guess)
